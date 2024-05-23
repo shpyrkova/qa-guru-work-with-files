bf3ae26d210f989b70c328908da69511c4959728
Daria Surkova <moe_more@list.ru>
2024-05-23 13:50:30 +0700

    change assertions in json tests

--- src/test/java/JsonJacksonTests.java
+++ src/test/java/JsonJacksonTests.java
@@ -1,11 +1,12 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import model.Pet;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStreamReader;
 import java.io.Reader;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class JsonJacksonTests {
 
@@ -14,17 +15,15 @@ public class JsonJacksonTests {
     @Test
     @DisplayName("Проверка json объекта")
     void jsonFileParsingImprovedTest() throws Exception {
-        try (Reader reader = new InputStreamReader(cl.getResourceAsStream("pet.json")
-        )) {
-
+        try (Reader reader = new InputStreamReader(cl.getResourceAsStream("pet.json"))) {
             ObjectMapper objectMapper = new ObjectMapper();
             Pet actual = objectMapper.readValue(reader, Pet.class);
-            Assertions.assertEquals("Doosya", actual.getName());
-            Assertions.assertEquals("Dog", actual.getSpecies());
-            Assertions.assertEquals(2014, actual.getBirthYear());
-            Assertions.assertEquals("brown", actual.getColour());
-            Assertions.assertArrayEquals(new String[]{"meat", "porridge"}, actual.getFavFoods());
 
+            assertThat(actual.getName()).isEqualTo("Doosya");
+            assertThat(actual.getSpecies()).isEqualTo("Dog");
+            assertThat(actual.getBirthYear()).isEqualTo(2014);
+            assertThat(actual.getColour()).isEqualTo("brown");
+            assertThat(actual.getFavFoods()).isEqualTo(new String[]{"meat", "porridge"});
         }
     }
 }
